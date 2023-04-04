a295cfc21e81199119468fec90718601d5235cf0
Simon Löschke <simon-loeschke@posteo.de>
2023-04-04 22:04:30 +0200

    Masseur: Datenbankverbindung aufgebaut und Funktionen erstellt

--- app/classes/masseur.py
+++ app/classes/masseur.py
@@ -4,8 +4,35 @@ import psycopg2
 
 
 class masseur:
-    # fragt in der postgresdatenbank massagestudio aus der Tabellle alle masseure ab und speichert sie in einer liste 
+    """Klasse Masseur: Datenbankobjekt, dass Masseure abfragt, erstellt und löscht
+    """    
+     
     
     def __init__(self) -> None:
         try:
-            database = postgres.Postgres()
+            db = psycopg2.connect(database='massagestudio',host='localhost', user='postgres', password='password', port='9988')
+            print("Datenbankverbindung hergestellt")
+        except:
+            print("Keine DB-Verbindung hergestellt")
+    
+    def search(self, name: str):
+        """Sucht nach einem bestimmten Masseur
+
+        Args:
+            name (str): Name des Masseurs
+
+        Returns:
+            _type_: Liste mit den gefundenen Masseuren
+        """        
+        self.cursor = self.db.cursor()
+        self.cursor.execute(f"SELECT * FROM masseur WHERE name = '{name}';")
+        
+        return self.cursor.fetchall()
+    
+    
+        
+        
+            
+
+if __name__=='__main__':
+    test = masseur()
