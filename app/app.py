08969a59e29132d85b36fabc66b4e0906e271103
Simon Löschke <simon-loeschke@posteo.de>
2023-04-05 21:50:53 +0200

    Tkinter angefangen, Masseure fertig gestellt,

--- app/app.py
+++ app/app.py
@@ -3,8 +3,23 @@ import classes
 
 
 def main():
-    pass
-
-
+    root = tkinter.Tk()
+    root.title("Massagestudio")
+    root.geometry("800x600")
+    root.resizable(False, False)
+    masseur = classes.masseur()
+    name = 'Hans'
+    suche = masseur.search(name)
+    button_masseure = tkinter.Button(root, text="Masseure", command=suche)
+    button_masseure.pack()
+    
+    root.mainloop()
+    
+    # Ein Button Masseure wird angelegt, beim Klicken soll die Funktion masseur.search aufgerufen werden
+    # masseur.search soll eine Liste aller Masseure ausgeben
+    # Die Liste soll in einem neuen Fenster angezeigt werden
+    list = masseur.search(name)
+    
+   
 if __name__=='__main__':
     main()
