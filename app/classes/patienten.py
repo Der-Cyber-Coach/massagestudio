0471b0bc36f7d94773a204e80c813f15b2af80c6
Simon Löschke <simon-loeschke@posteo.de>
2023-04-06 09:18:19 +0200

    patientenklasse initiiert

--- app/classes/patienten.py
+++ app/classes/patienten.py
@@ -1,5 +1,44 @@
 import psycopg2
+import os
 
 class patienten:
-    pass
+    '''Klasse, die ein Patientenobjekt erstellt'''
+    def __init__(self) -> None:
+        try:
+            schema = 'massagestudio'
+            self.db = psycopg2.connect(database='postgres',host='localhost', user='postgres', password='password', port='9988',options=f"-c search_path={schema}")
+            print("200 OK")
+        except:
+            self.db.close()
+            print("500 Datenbankverbindung gescheitert")
+            os._exit(1)
+    
+    # Patientensuche
+    
+    def search(self,name:str,patientennummer:int):
+        pass
+    
+    
+    
+    def create(self, name:str):
+        pass
+    
+    
+    def update(self, name_alt:str, name_neu: str, patientennummer:int):
+        pass
+    
+    
+    
+    
+
+
+
+
+
+
+
+if __name__=='__main__':
+    patient = patienten()
+
+            
 
