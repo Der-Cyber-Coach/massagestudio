4201c0a0f47ee6df203164fe477e2016851ae659
Simon Löschke <simon-loeschke@posteo.de>
2023-04-07 15:23:55 +0200

    Räume Fixes #4

--- app/classes/gui.py
+++ app/classes/gui.py
@@ -5,7 +5,7 @@ from .patienten import *
 from .raum import *
  
 
-class view(masseur,patienten):
+class view(raum):
      
     def __init__(self):
         super().__init__()
@@ -30,7 +30,7 @@ class view(masseur,patienten):
         return self.root
         
     def raum(self):
-        self.raum = tkinter.Toplevel()
+        self.raum = tkinter.Tk()
         self.raum.title("Raum")
         self.raum.geometry("800x600")
         self.raum.resizable(False, False)
@@ -39,25 +39,43 @@ class view(masseur,patienten):
     
     def masseure(self):
         
+        self.masseur= masseur()
         # Fensterkonfiguration
         self.masseure = tkinter.Toplevel()
         self.masseure.title("Masseure")
         self.masseure.geometry("800x600")
         self.masseure.resizable(False, False)
         self.masseure.configure(bg="white")
+       # self.masseur_erstellen = self.masseur.create('Test')
+        self.masseur_löschen = self.masseur.delete('Test')
+        self.masseur_ändern = self.masseur.update('Test', 'Test2')
+        self.masseur_suchen = self.masseur.search('Test')
         
         
-        
+        # Button funktionen: Funktionen müssen definiert werden, damit sie beim Klicken aufgerufen werden können
+        def masseur_erstellen():
+            self.masseur_erstellen = self.masseur.create('Test')
+        def masseur_löschen():
+            self.masseur_löschen = self.masseur.delete('Test')
+        def masseur_ändern():
+            self.masseur_ändern = self.masseur.update('Test', 'Test2')
+        def masseur_suchen():
+            self.masseur_suchen = self.masseur.search('Test')
+            
         # Buttons
-        self.button_masseur_erstellen = tkinter.Button(self.masseure, text="Masseur erstellen")
-        self.button.masseur_löschen = tkinter.Button(self.masseure, text="Masseur löschen", command=self.masseur_löschen)
-        self.button.masseur_ändern = tkinter.Button(self.masseure, text="Masseur ändern", command=self.masseur_ändern)
-        self.button.masseur_suchen = tkinter.Button(self.masseure, text="Masseur suchen", command=self.masseur_suchen)
-        self.button.zurück = tkinter.Button(self.masseure, text="Zurück", command=self.root)
+        self.button_masseur_erstellen = tkinter.Button(self.masseure, text="Masseur erstellen", command=masseur_erstellen)
+        self.button_masseur_löschen = tkinter.Button(self.masseure, text="Masseur löschen", command=masseur_löschen)
+        self.button_masseur_ändern = tkinter.Button(self.masseure, text="Masseur ändern", command=masseur_ändern)
+        self.button_masseur_suchen = tkinter.Button(self.masseure, text="Masseur suchen", command=masseur_suchen)
         
         
         
+        # Buttons packen
+        self.button_masseur_löschen.pack()
+        self.button_masseur_ändern.pack()
+        self.button_masseur_suchen.pack()
         self.button_masseur_erstellen.pack()
+        
         return self.masseure
     
     
